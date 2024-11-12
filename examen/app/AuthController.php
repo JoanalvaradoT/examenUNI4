f25c715d454c998104f3b78574fd17e28eabf368
JoanAlvaradoT <alvaradotellesf@gmail.com>
2024-11-11 23:04:03 -0700

    sidebarYnavbarNuevosArchivos

--- examen/app/AuthController.php
+++ examen/app/AuthController.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($response['code']) && $response['code'] == 2) {
         $_SESSION['user'] = $response['data']['email'];  
         $_SESSION['token'] = $response['data']['token'];  
-        header("Location: /unidad4/examen/tpm/index.html");  
+        header("Location: /unidad4/examen/tpm/dashboard/index.html");  
         exit();  
     } else {
         $_SESSION['error'] = $response['message'] ?? "Credenciales incorrectas.";
